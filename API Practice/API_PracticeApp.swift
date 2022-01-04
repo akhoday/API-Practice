07e3755c201a13a08a90c15dc986c66c531e72fd
Nathalie Borden <s016602@students.lmsd.org>
2022-01-04 08:13:30 -0500

    Initial Commit

---
+++ API Practice/API_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_PracticeApp.swift
+//  API Practice
+//
+//  Created by Nathalie Borden (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct API_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
