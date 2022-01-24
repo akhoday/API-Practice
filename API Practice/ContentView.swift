dbcdbc10bd6d6130f6411a9aedf5e923888f42a3
Nathalie Borden <s016602@students.lmsd.org>
2022-01-24 08:45:22 -0500

    Changed ContentView to a basic list as a framework, possibly changed the API call to include daily info but I forget if it was already like that lol

--- API Practice/ContentView.swift
+++ API Practice/ContentView.swift
@@ -8,27 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
- 
-        @StateObject var fetchData = FetchData()
-         
-         var body: some View {
-             List(fetchData.responses.currents){ current in
-                 NavigationLink( 
-                     destination: SwiftUIWebView(url: current.url),
-                    label: {
-                        HStack{
-                            KFImage(current.urlToImage)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                            
-                            Text(current.temp!)
-                             }
-                 })
-             }
-         }
+    var body: some View {
         
+        var mainList : [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
         
+        NavigationView {
+            List(mainList.indices) {something in
+                
+                
+                Text(mainList[something]).frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).border(Color.red, width: 4)
+            
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- API Practice/DataParsing.swift
+++ API Practice/DataParsing.swift
@@ -13,7 +13,7 @@ class FetchData : ObservableObject{
     init(){
         // this code here decodes the JSON in a much simpliler process
         
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=40.025&lon=-75.2829&exclude=alerts,minutely,hourly&appid=84093e05f55a9182393f95986f3b9d57")!
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=40.025&lon=-75.2829&units=imperial&exclude=alerts,minutely,hourly&appid=84093e05f55a9182393f95986f3b9d57")!
         
         URLSession.shared.dataTask(with: url) {(data, response, errors) in
             guard let data = data else {
@@ -72,5 +72,5 @@ extension Current: Identifiable{
 }
 
 extension Weather: Identifiable{
-    var id: String {return main!}
+
 }
